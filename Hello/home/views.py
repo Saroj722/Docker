4155eeef132c9230075836d84f1967673e420c85
Saroj722 <155639348+Saroj722@users.noreply.github.com>
2025-06-17 12:55:55 +1000

    Add files via upload

---
+++ Hello/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    # return HttpResponse("this is homepage")
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse("this is about")
+def contact(request):
+    return render(request, 'contact.html')
+    # return HttpResponse("this is contact")
+def services(request):
+    return render(request, 'services.html')
+    # return HttpResponse("this is services")
